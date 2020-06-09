e6fc92d2e49de366d5cf7571ad258081f274cae5
Chetna Warkade <chetna.warkade@gmail.com>
2020-06-10 00:40:54 +0530

    Fixed mistake in optimised count occurence

--- BinarySearch/count_occurence1.cpp
+++ BinarySearch/count_occurence1.cpp
@@ -1,6 +1,6 @@
 int binarySearch(const vector<int> &A, int B, bool searchFirst){
     int low = 0, high = A.size()-1;
-    int result = 0;
+    int result = -1;
     while(low <= high){
         int mid = low+(high-low)/2;
         if(A[mid] == B){
@@ -22,5 +22,8 @@ int Solution::findCount(const vector<int> &A, int B) {
     
     int start = binarySearch(A,B, true);
     int end = binarySearch(A,B, false);
+	if(start == -1)
+		return 0;
+	else
     return end - start + 1;
 }
